4dae65b5bef2d6a3e1330cc697ee7f41970d0889
Alfarizi <ccfarizi@gmail.com>
2021-06-23 05:18:39 +0700

    Fix Auth Login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Auth extends CI_Controller {
 
 
-	public function index()
+	public function login()
 	{
 		$this->load->view('auth/login');	
 	}
--- application/views/Auth/login.php
+++ application/views/Auth/login.php
@@ -37,7 +37,7 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="<?=base_url();?>assets/index2.html" method="post">
+    <form action="<?=site_url('auth/proses');?>" method="post">
       <div class="form-group has-feedback">
         <input type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
