2691b0b73ce37eed6d4e035e89625f39b642ab0c
Alfarizi <ccfarizi@gmail.com>
2021-06-23 04:16:06 +0700

    Dinamis Template
    Template yang dinamis dengan library

---
+++ application/controllers/Supplier.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Supplier extends CI_Controller {
+
+    public function index()
+    {
+        $this->template->load('template','supplier/supplier_data ');
+    }
+
+}
+
+/* End of file Supplier.php */
